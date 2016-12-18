8677bcd4b4a253a0383df0c88eb50731b848e2fa
zzgo <zzgocc@gmail.com>
2016-12-18 21:50:42 +0800

    1.1

--- Home/Home/HomeViewController.m
+++ Home/Home/HomeViewController.m
@@ -6,6 +6,7 @@
 //  Copyright © 2016年 zzgo. All rights reserved.
 //
 
+
 #import "HomeViewController.h"
 
 @implementation HomeViewController
@@ -13,6 +14,22 @@
 -(void)viewDidLoad{
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor orangeColor];
+    UIButton *button=[UIButton new];
+    
+    CGSize size=self.view.frame.size;
+    
+    button.frame=CGRectMake((size.width-100)/2,( size.height-44)/2, 100, 44);
+    button.backgroundColor=[UIColor orangeColor];
+    [button setTitle:@"back" forState:UIControlStateNormal];
+    [button addTarget:self 
+               action:@selector(touchBackButton:)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
+}
+
+-(void)touchBackButton:(UIButton *)sender{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
